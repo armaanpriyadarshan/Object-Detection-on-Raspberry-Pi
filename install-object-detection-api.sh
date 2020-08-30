09ce8c048ff5c5fca131e19eebadc32350dcaaf2
Armaan Priadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2020-08-30 17:02:57 -0400

    Update install-object-detection-api.sh

--- install-object-detection-api.sh
+++ install-object-detection-api.sh
@@ -1,1 +1,10 @@
+# Clone the TensorFlow Models Repository, Compile the Protos, Add Object Detection Module to Path
 
+git clone https://github.com/tensorflow/models.git
+cd models/research
+protoc object_detection/protos/*.proto --python_out=.
+export PYTHONPATH=$PYTHONPATH:/home/pi/tensorflow/models/research:/home/pi/tensorflow/models/research/slim
+
+# Echo Success Message
+
+echo TensorFlow Object Detection API Setup Successfully!
