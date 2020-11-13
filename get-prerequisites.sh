e3e5e8594ac1ebb4c51acfba93a461b187e1ad31
Armaan Priadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2020-11-13 09:32:35 -0500

    Update get-prerequisites.sh

--- get-prerequisites.sh
+++ get-prerequisites.sh
@@ -1,6 +1,6 @@
 # Installs Prerequisites for OpenCV
 
-sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-100
+sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
 sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
 sudo apt-get install libatlas-base-dev
 sudo apt-get install libjasper-dev
